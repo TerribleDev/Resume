22043b73dc02963e31a26c9c421c14a5f90c3325
Tommy Parnell <tparnell8@gmail.com>
2015-11-20 23:44:40 -0500

    better file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 rm -rf .git
-npm install
-node ./node_modules/resume-cli/index.js export index -f html --theme elegant
 git config user.email "tparnell8@gmail.com"
 git config user.name "Tommy Parnell"
 git init
