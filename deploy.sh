b1d74da37cba3c910adc4efed3be09d9b1930250
Tommy Parnell <tparnell8@gmail.com>
2015-11-20 23:59:27 -0500

    adjust dep

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-rm -rf ./.git
 git config user.email "tparnell8@gmail.com"
 git config user.name "Tommy Parnell"
-git init
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force "git@github.com:tparnell8/Resume.git" master:gh-pages
