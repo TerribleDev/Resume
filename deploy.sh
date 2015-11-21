370bdbb1eb2ab166014e43cadce6625396187dbc
Tommy Parnell <tparnell8@gmail.com>
2015-11-21 00:04:50 -0500

    hit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-git config user.email "tparnell8@gmail.com"
-git config user.name "Tommy Parnell"
+echo "hit"
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force "git@github.com:tparnell8/Resume.git" master:gh-pages
